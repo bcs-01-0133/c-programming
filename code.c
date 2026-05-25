04bca85f9cae874b183dbe553d8ab5a6704759de
bcs-01-0133 <s56612432@gmail.com>
2026-05-25 13:43:26 +0300

    Add files via upload

---
+++ code.c
@@ -0,0 +1,29 @@
+//program displaying units offered this semester
+/*
+
+description:program displaying units offered this semester
+name:stephen ngugi
+reg no:bcs-01-0133/2026
+date:22-05-2026
+version 1
+*/
+
+#include<stdio.h>
+
+int main(){
+	
+     printf("unit offered this semester\n");
+
+//display units offered this semester;
+
+    printf("structured programming and algorithms\n");
+    printf("communication skills\n");
+    printf("introduction to physics\n");
+    printf("digital literacy\n");
+    printf("basic mathematics\n");
+    printf("health and wellness management\n");
+    printf("computational logics\n");
+
+return 0;
+
+}
