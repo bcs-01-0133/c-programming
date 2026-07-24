9cfac08863ec197fb6239a87eebb7e5f84212de9
stephenngugi <s56612432@gmail.com>
2026-07-24 16:08:24 +0300

    Add files via upload

---
+++ week 1 units offerd this semester.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+	//Units offerd this semester
+	
+	printf("basic to mathematics\n");
+		printf("structure programming\n");
+			printf("computational logis\n");
+				printf("intoduction to physics\n");
+					printf("digital literacy\n");
+						printf("communication skills\n");
+							printf("health and wellness \n");
+								
+								return 0;
+}
