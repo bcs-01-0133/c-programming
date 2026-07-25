47c1be824c260440a3ac3d2b574f70babda7ab9a
stephenngugi <s56612432@gmail.com>
2026-07-25 14:20:30 +0300

    Add files via upload

---
+++ week 1 task 2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/*
+name:stephen ngugi
+adm:bcs-01-0133/2026
+*/
+int main(){
+	
+	int bank_balance;
+	int phone_number;
+	int height_m;
+	
+	
+	printf("enter height_m\n");
+	scanf("%d",&height_m);
+	
+	printf("enter phone_number");
+	scanf("%d",&phone_number);
+	
+	printf("enter bank_balance");
+	scanf("%d",&bank_balance);
+	
+	return 0;
+	
+}
